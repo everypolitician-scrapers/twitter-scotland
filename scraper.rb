4e840a471da1040ddab9d15cabd8bd9d1d247adc
Tony Bowden <tony@tmtm.com>
2017-08-11 09:46:50 +0100

    Make this scraper equivalent to Swiss version
    This one is failing on morph, whereas that one is passing. Harmonise
    both as much as possible to try to isolate the problem.

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,15 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'rubygems'
+require 'bundler/setup'
+
 require 'scraperwiki'
 require 'twitter_list'
-                              
+
 abort "Need to set ENV['MORPH_TWITTER_TOKENS']" unless ENV.key? 'MORPH_TWITTER_TOKENS'
 twitter_list = TwitterList::Scraper.new(twitter_tokens: ENV['MORPH_TWITTER_TOKENS'])
-ScraperWiki.save_sqlite([:id], twitter_list.people('ScotParl', 'msps-session-5'))
+
+data = twitter_list.people('ScotParl', 'msps-session-5')
+warn data
+ScraperWiki.save_sqlite([:id], data)
