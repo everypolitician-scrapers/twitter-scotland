f7c2cd61c031d27e512bc20a65428e32eb93fdfa
Tony Bowden <tony@tmtm.com>
2017-08-11 09:58:07 +0100

    remove debugging output

--- scraper.rb
+++ scraper.rb
@@ -10,6 +10,4 @@ require 'twitter_list'
 abort "Need to set ENV['MORPH_TWITTER_TOKENS']" unless ENV.key? 'MORPH_TWITTER_TOKENS'
 twitter_list = TwitterList::Scraper.new(twitter_tokens: ENV['MORPH_TWITTER_TOKENS'])
 
-data = twitter_list.people('ScotParl', 'msps-session-5')
-warn data
-ScraperWiki.save_sqlite([:id], data)
+ScraperWiki.save_sqlite([:id], twitter_list.people('ScotParl', 'msps-session-5'))
